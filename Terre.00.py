5a5773389e033210a124ea113f6ec7d40fc3a94a
ZimtecHH <110943161+ZimtecHH@users.noreply.github.com>
2022-08-26 16:43:17 +0200

    Add files via upload

---
+++ Terre.00.py
@@ -0,0 +1,5 @@
+# Créez un programme qui affiche l’alphabet en lettres minuscules suivi d’un retour à la ligne
+# utiliser boucle
+
+for i in ["A b C d e f g h i J k l M n o p q r s t u v w x y Z"]:
+    print("%s \n" % i.lower())
