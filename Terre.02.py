b39bf6332a3aadb2bf5763a363cace2be34ffe4e
ZimtecHH <110943161+ZimtecHH@users.noreply.github.com>
2022-08-27 22:03:12 +0200

    Add files via upload

--- Terre.02.py
+++ Terre.02.py
@@ -1,8 +1,18 @@
 #Créez un programme qui affiche les arguments qu’il reçoit ligne par ligne, peu importe le nombre d’arguments.
-argument1 = input("Entrez nombre d'argument voulu : ")
+
+argument1 = [input("Entrez nombre d'argument voulu : ")]
 
 primes = argument1
 
-for prime in primes:
-    coupeur = prime.split("\n")
-    print(coupeur)
+for number in argument1:
+    if number == 'avion':
+        break
+
+    if number == 'auhkhukh':
+        continue
+
+    number = number.split(" ")
+
+    primes = number
+    for prime in primes:
+        print(prime)
