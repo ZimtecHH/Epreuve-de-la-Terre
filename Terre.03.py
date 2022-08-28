7eb7e766bb20ef1097bff1484b0c54e1bb27448a
ZimtecHH <110943161+ZimtecHH@users.noreply.github.com>
2022-08-28 16:20:19 +0200

    Add files via upload

--- Terre.03.py
+++ Terre.03.py
@@ -1,12 +1,62 @@
 #Créez un programme qui affiche
 # l’alphabet à partir de la lettre donnée en argument en
 # lettres minuscules suivi d’un retour à la ligne.
-a=1; b=2 ; c=3 ; d=4 ; e=5 ; f=6 ; g=7 ; h=8 ; i=9 ; j=10 ; k=11 ; l=12 ; m=13 ; n=14 ; o=15 ; p=16 ; q=17 ;
-r=18 ; s=19 ; t=20 ; u=21 ; v=22 ; w=23 ; x=24 ; y=25 ; z=26 ;
+
+astring = "abcdefghijklmnopqrstuvwxyz"
 
 alphabet = input("saisir une lettre de l'alphabet : " )
 
-while alphabet < 26:
-    print(alphabet)
-    alphabet += 1
 
+
+if alphabet == "a" :
+    print(astring[0:26])
+elif alphabet == "b" :
+    print(astring[1:26])
+elif alphabet == "c" :
+    print(astring[2:26])
+elif alphabet == "d" :
+    print(astring[3:26])
+elif alphabet == "e" :
+    print(astring[4:26])
+elif alphabet == "f" :
+    print(astring[5:26])
+elif alphabet == "g" :
+    print(astring[6:26])
+elif alphabet == "h" :
+    print(astring[7:26])
+elif alphabet == "i" :
+    print(astring[8:26])
+elif alphabet == "j" :
+    print(astring[9:26])
+elif alphabet == "k" :
+    print(astring[10:26])
+elif alphabet == "l" :
+    print(astring[11:26])
+elif alphabet == "m" :
+    print(astring[12:26])
+elif alphabet == "n" :
+    print(astring[13:26])
+elif alphabet == "o" :
+    print(astring[14:26])
+elif alphabet == "p" :
+    print(astring[15:26])
+elif alphabet == "q" :
+    print(astring[16:26])
+elif alphabet == "r" :
+    print(astring[17:26])
+elif alphabet == "s" :
+    print(astring[18:26])
+elif alphabet == "t" :
+    print(astring[19:26])
+elif alphabet == "u" :
+    print(astring[20:26])
+elif alphabet == "v" :
+    print(astring[21:26])
+elif alphabet == "w" :
+    print(astring[22:26])
+elif alphabet == "x" :
+    print(astring[23:26])
+elif alphabet == "y" :
+    print(astring[24:26])
+elif alphabet == "z" :
+    print(astring[25:26])
