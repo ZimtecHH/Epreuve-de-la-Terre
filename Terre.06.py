9e80188cae4909fb183c6509cc72ea17c5e26197
ZimtecHH <110943161+ZimtecHH@users.noreply.github.com>
2022-08-31 22:28:38 +0200

    Add files via upload

---
+++ Terre.06.py
@@ -0,0 +1,6 @@
+#Créez un programme qui affiche l’inverse
+# de la chaîne de caractères donnée en argument.
+
+Inverser = input("Rentrer une chaine de caractère à inverser : ")
+
+print(Inverser[::-1])
