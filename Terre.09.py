7062fa7ca1b481bea4374b206004c03985baba8d
ZimtecHH <110943161+ZimtecHH@users.noreply.github.com>
2022-09-02 00:01:51 +0200

    Add files via upload

---
+++ Terre.09.py
@@ -0,0 +1,10 @@
+#Créez un programme qui affiche
+# la racine carrée d’un entier positif.
+
+Nombre = int(input(" Rentrez l'entier : "))
+
+if Nombre < 0:
+   print(" négatif erreur ")
+
+else:
+   print(Nombre**0.5)
