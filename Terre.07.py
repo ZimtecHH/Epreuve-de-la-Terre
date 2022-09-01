867708c450cc157a4e48adb3702a5c956704ee8d
ZimtecHH <110943161+ZimtecHH@users.noreply.github.com>
2022-09-01 22:57:44 +0200

    Add files via upload

---
+++ Terre.07.py
@@ -0,0 +1,9 @@
+#Créez un programme qui affiche le nombre
+# de caractères d’une chaîne de caractères
+# passée en argument.
+
+
+
+astring = input(" Rentrez une chaine de caractère : ")
+
+print(len(astring))
