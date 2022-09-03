8d04add88ccb7129c4faa7b5a0dc677453473f6b
ZimtecHH <110943161+ZimtecHH@users.noreply.github.com>
2022-09-03 14:14:14 +0200

    Add files via upload

---
+++ Terre.10.py
@@ -0,0 +1,20 @@
+#Créez un programme qui
+# affiche si un nombre est premier ou pas.
+
+Nombre = int(input(" Rentrez le nombre : "))
+
+
+if Nombre > 1:
+    for i in range(2, int(Nombre/2)+1):
+         if (Nombre % i) == 0:
+            print("Ce n'est pas un nombre premier ")
+            break
+    else:
+        print("C'est un nombre premier ")
+
+else:
+    print("Ce n'est pas un nombre premier ")
+
+
+
+#LISTE DE NOMBRE PREMIER : 2, 3, 5, 7, 11, 13, 17, 19,
